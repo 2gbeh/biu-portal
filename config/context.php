e9d2167b1d30a51f6fb0b0f2b81410907b2e46d7
Tugbeh Emmanuel <41299045+2gbeh@users.noreply.github.com>
2023-01-18 17:50:37 +0100

    Add files via upload

---
+++ config/context.php
@@ -0,0 +1,159 @@
+<?php
+
+return [
+    'name' => 'Benson Idahosa University',
+    'alias' => 'BIU Portal',
+    'title' => 'BIU Portal &bull; Benson Idahosa University',
+    'powered_by' => '<var>v2.3</var> Powered by <strong>Pear SDC</strong> <sup>&reg;</sup>',
+    'copyright' => sprintf('&copy; %s Benson Idahosa University. P.M.B 1100', date('Y')),
+    'website' => "https://biu.edu.ng/",
+    'website_f' => '<a href="https://biu.edu.ng/" class="underline">https://biu.edu.ng/</a>',
+    'phone' => "+2348149906200",
+    'phone_f' => "+234(0)81-4990-6200",
+    'email' => "info@biu.edu.ng",
+    'email_2' => "ictdirector@biu.edu.ng",
+    'address' => 'PMB 1100, University Way, Off Upper Adesuwa Road, G.R.A, Benin City, Edo State, Nigeria.',
+    'address_f' => '<a href="https://biu.edu.ng/" class="underline">Private Mail Box (P.M.B) 1100,</a> <br />
+                    University Way, Off Upper Adesuwa Road, G.R.A, <br />
+                    Benin City, Edo State, <br/>
+                    Nigeria.',
+    'theme' => [
+        'gold' => '#dca825',
+        'blue' => '#233066',
+        'blue_hover' => '#131e49',
+        'blue_focus' => '#0a2049',
+        'black' => '#2d2d2d',
+        'pear' => '#82c341',
+    ],
+    'contact_us' => "Contact our Technical Support Team via the links provided below, also browse the Frequently Asked Questions (FAQs) section for swift answers to common challenges that may arise when using our new and improved portal.",
+    'vision' => 'Benson Idahosa University aspires to be model for Academics, Professionals and Entrepreneurs, who will be effective disciples for Christ by excelling in their professional fields. It will be distinguished by high performance in interdisciplinary research, addressing national and international problems, eliminating wrong behavioural patterns among students and becoming a storehouse of knowledge to be used for benefit of mankind on Christian ethical principles designed to change the nation and the continent by changing one student’s life at a time.',
+    'mission' => 'Benson Idahosa University is a private Christian University providing state-of-the-art undergraduate, postgraduate and professional education. We are committed to the mission of raising leaders for the nation, who are complete in spirit, mind and body, thus contributing to the production of high-level leadership and quality manpower for the nation and the world. We perform these functions by judiciously utilising current information and communication technology, networking with similar institutions worldwide, putting people first in operations and emphasizing Christian ethical values. The beneficiaries of the University’s service are students, employers of labour, present and future generations, Nigeria and the global community.',
+    'core_values' => [
+        'Teamwork',
+        'Ownership Mentality',
+        'People Matter',
+        'Transparency & Integrity',
+        'Innovation',
+        'Accountability',
+        'Academic excellence with Godliness',
+    ],
+    'core_purpose' => 'Change Nigeria',
+    'social' => [
+        [
+            'a' => [
+                'href' => 'https://www.facebook.com/biuNigeria',
+                'title' => 'Facebook Page',
+            ],
+            'i' => 'fab fa-facebook',
+        ],
+        [
+            'a' => [
+                'href' => 'https://twitter.com/biuNigeria',
+                'title' => 'Twitter Handle',
+            ],
+            'i' => 'fab fa-twitter',
+        ],
+        [
+            'a' => [
+                'href' => 'https://www.youtube.com/channel/UCKsSi1AAkfyC7o75RtHzljQ',
+                'title' => 'YouTube Channel',
+            ],
+            'i' => 'fab fa-youtube',
+        ],
+        [
+            'a' => [
+                'href' => 'https://www.linkedin.com/school/benson-idahosa-university/',
+                'title' => 'LinkedIn Profile',
+            ],
+            'i' => 'fab fa-linkedin',
+        ],
+    ],
+    'apps' => [
+        [
+            'fi' => 'fi fi-rs-graduation-cap',
+            'fa' => 'fas fa-user-graduate',
+            'a' => [
+                'href' => "/user/student",
+                'text' => 'Student Portal',
+                'text_alt' => 'Student',
+            ],
+            'p' => 'Hostel Accommodation, Hostel Maintenance, Health Clinic, Cafeteria Services, and Off Campus permits. Designed to attend to the welfare and general wellbeing of the students of BIU. ',
+        ],
+        [
+            'fi' => 'fi fi-rs-id-badge',
+            'fa' => 'fas fa-id-card',
+            'a' => [
+                'href' => "/admin",
+                'text' => 'Staff Portal',
+                'text_alt' => 'Staff',
+            ],
+            'p' => 'BIU Staff handles a well-developed academic programmes which provides opportunities for students active participation and involvement in our student leadership programs.',
+        ],
+        [
+            'fi' => 'fi fi-rs-diploma',
+            'fa' => 'fas fa-award',
+            'a' => [
+                'href' => "/user/applicant",
+                'text' => 'Admission Portal',
+                'text_alt' => 'Admission',
+            ],
+            'p' => 'Welcome to an opportunity of a unique learning experience with challenging curriculum that can make you lead anywhere in the world. At BIU you will find the total university experience.',
+        ],
+        [
+            'fi' => 'fi fi-rs-interactive',
+            'fa' => 'fas fa-laptop-code',
+            'a' => [
+                'href' => 'http://cbt.biu.edu.ng/',
+                'target' => "_new",
+                'text' => 'CBT Demo',
+                'text_alt' => 'CBT Demo',
+            ],
+            'sup' => ['class' => 'badge', 'text' => 'New'],
+            'p' => 'Our CBT platform gives you access to over 60,000 past questions in a variety of subjects with a better CBT experience, and 100% study offline, with free video lessons.',
+        ],
+        [
+            'fi' => 'fi fi-rs-globe',
+            'fa' => 'fas fa-globe',
+            'a' => [
+                'href' => 'https://biu.edu.ng/',
+                'target' => "_new",
+                'text' => 'BIU Website',
+                'text_alt' => 'Website',
+            ],
+            'p' => '<a href="https://biu.edu.ng/" class="underline">Private Mail Box (P.M.B) 1100,</a> <br />
+                        University Way, Off Upper Adesuwa Road, G.R.A, <br />
+                        Benin City, Edo State, Nigeria.',
+        ],
+        [
+            'fi' => 'fi fi-rs-smartphone',
+            'fa' => 'fab fa-google-play',
+            'a' => [
+                'href' => 'https://play.google.com/store/apps/details?id=edu.biu.mobileapp',
+                'target' => "_new",
+                'text' => 'BIU App',
+                'text_alt' => 'BIU App',
+
+            ],
+            'sup' => ['class' => 'beta', 'text' => 'BETA'],
+            'p' => 'Mobile app for Benson Idahosa university (BIU) to enable students easily get information they need and send messages to the student experience team.',
+        ],
+    ],
+    'tenants' => [
+        'APPLICANT',
+        'STUDENT',
+        'STAFF',
+    ],
+    'routes' => [
+        'admin' => 'BIU Staff Portal',
+        'admin/*' => 'BIU Staff Portal',
+        'user/student' => 'BIU Student Portal',
+        'user/student/*' => 'BIU Student Portal',
+        'user/applicant' => 'BIU Admission Portal',
+        'user/applicant/*' => 'BIU Admission Portal',
+    ],
+    'domain' => 'biuportal.org',
+    'url' => 'https://biuportal.org',
+    'cpanel' => 'https://biuportal.org/cpanel',
+    'webmail' => 'https://biuportal.org/webmail',
+    'payment' => 'https://business.quickteller.com/signin',
+];
